8a2d012369ff6aed2f6a6aeb7da653de2a170e55
James Harr <james.harr@gmail.com>
2014-02-11 09:23:52 -0600

    Better examples

--- sorter_test.go
+++ sorter_test.go
@@ -21,8 +21,20 @@ func TestSort_fields(t *testing.T) {
 	slc := collections.Copy(samples).([]Person)
 
 	s := collections.Sorter{}
+
+	// s.By(func(a interface{})interface{}{
+	//    return a.(Person).Room
+    // })
 	s.ByField("Room")
+
+	// Value sort (default): compare normal types
+	// Natural sort: "foo-2.30" < "foo-2.1"  < "foo-010.3"
+	// Version sort: "foo-2.1"  < "foo-2.30" < "foo-010.3"
 	s.Natural()
+
+	// Go sort this slice
 	s.Sort(slc)
+
+	// Debug log since there's really no unit tests
 	t.Log(slc)
 }
